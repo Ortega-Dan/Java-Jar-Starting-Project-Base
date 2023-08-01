87f227a89169f80b8afe387e81a6fb7cc344685b
Dan Ortega <ortega.dan2010@gmail.com>
2023-08-01 11:21:48 -0500

    cleaner test class

--- src/test/java/org/danort/test/RunnerTest.java
+++ src/test/java/org/danort/test/RunnerTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class RunnerTest {
+class RunnerTest {
 
     @Test
     void testRunner() {
